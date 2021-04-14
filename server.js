87293d7a4cb400215db1650a4ba63a74949a0b1d
emran Muhammad aloul <78364500+emranaloul@users.noreply.github.com>
2021-04-14 21:47:32 +0300

    Update server.js
    activating PG

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ server.set( 'view engine', 'ejs' );
 server.use( express.static( './public' ) );
 server.use(methodOverride('_method'));
 
-const client = new pg.Client( process.env.DATABASE_URL );
-// const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false}} );
+// const client = new pg.Client( process.env.DATABASE_URL );
+const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false}} );
 
 
 server.use( express.urlencoded( {extended:true} ) );
